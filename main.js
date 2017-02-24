d0aacbae8e68854ebc8a2c29bcf27c51cde1d7df
KAUNET <eri6640@gmail.com>
2017-02-24 14:56:11 +0200

    mainjs

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const port = 8090;
 
 app.use(express.static('res'));
 
-app.use("/js", express.static(__dirname + "/../resources/js"));
-app.use("/img", express.static(__dirname + "/../resources/img"));
-app.use("/css", express.static(__dirname + "/../resources/css"));
-app.use("/fonts", express.static(__dirname + "/../resources/fonts"));
+app.use("/js", express.static( __dirname + "/../resources/js" ));
+app.use("/img", express.static( __dirname + "/../resources/img" ));
+app.use("/css", express.static( __dirname + "/../resources/css" ));
+app.use("/fonts", express.static( __dirname + "/../resources/fonts" ));
 
-app.use("/page", express.static(__dirname + "/../templates"));
+app.use("/page", express.static( __dirname + "/../templates" ));
 
 app.all("/*", function(req, res){
 	res.sendFile( __dirname + "/res/" + "index.html" );
