fb23b11bd85886fa7707407d64c065f77a190964
richs95 <ricards.sotaks@gmail.com>
2017-03-10 11:26:48 +0200

    registratio changes

--- res/resources/js/controllers/registration.js
+++ res/resources/js/controllers/registration.js
@@ -1,7 +1,25 @@
 var app = angular.module( 'COREAPI', [] );
 app.controller( 'RegController', function( $scope, $http, $window ) {
-	
-	$scope.hhhh = 'test';
-	  
-	  
+
+	$scope.errorData = {
+		error : false,
+		message : 'error?'
+	};
+
+	$scope.doReg = function(){
+
+		var username = $scope.username;
+		var email = $scope.email;
+		var password = ...;
+
+		if( typeof username === 'undefined' || !username ){
+			$scope.errorData.error = true;
+			$scope.errorData.message = 'asdfd';
+
+		}
+
+
+
+	};
+
 });
