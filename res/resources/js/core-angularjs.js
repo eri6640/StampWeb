bf1e332c16e3aa879d8cd45a30fb91a9f48398f9
richs95 <ricards.sotaks@gmail.com>
2017-03-17 14:33:59 +0200

    angularjs config

--- res/resources/js/core-angularjs.js
+++ res/resources/js/core-angularjs.js
@@ -14,6 +14,7 @@ app.config( function( $routeProvider, $httpProvider, $locationProvider ) {
     $locationProvider.html5Mode( true );
 
     $routeProvider.when( '/home', {
+      requireAuth: true,
 	controller : 'HomeController',
 	templateUrl : 'templates/home.html',
 	resolve : {
@@ -27,6 +28,7 @@ app.config( function( $routeProvider, $httpProvider, $locationProvider ) {
     } );
 
     $routeProvider.when( '/login', {
+  requireAuth: false,
 	controller : 'LoginController',
 	templateUrl : 'templates/login.html',
 	resolve : {
@@ -40,6 +42,7 @@ app.config( function( $routeProvider, $httpProvider, $locationProvider ) {
     } );
 
     $routeProvider.when( '/registration', {
+      requireAuth: false,
 	controller : 'RegController',
 	templateUrl : 'templates/registration.html',
 	resolve : {
@@ -53,6 +56,7 @@ app.config( function( $routeProvider, $httpProvider, $locationProvider ) {
     } );
 
     $routeProvider.when( '/profile', {
+        requireAuth: true,
 	controller : 'ProfileController',
 	templateUrl : 'templates/profile.html',
 	resolve : {
@@ -66,6 +70,7 @@ app.config( function( $routeProvider, $httpProvider, $locationProvider ) {
     } );
 
     $routeProvider.when( '/search', {
+      requireAuth: true,
 	controller : 'SearchController',
 	templateUrl : 'templates/search.html',
 	resolve : {
@@ -79,55 +84,59 @@ app.config( function( $routeProvider, $httpProvider, $locationProvider ) {
     } );
 
     $routeProvider.when( '/stamp', {
-	controller : 'StampController',
-	templateUrl : 'templates/stamp.html',
-	resolve : {
-	    lazy : [ '$ocLazyLoad', function( $ocLazyLoad ) {
-		return $ocLazyLoad.load( [ {
-		    name : 'COREAPI',
-		    files : [ 'resources/js/controllers/' + 'stamp.js' ]
-		} ] );
-	    } ]
-	}
+      requireAuth: true,
+    	controller : 'StampController',
+    	templateUrl : 'templates/stamp.html',
+    	resolve : {
+    	    lazy : [ '$ocLazyLoad', function( $ocLazyLoad ) {
+    		return $ocLazyLoad.load( [ {
+    		    name : 'COREAPI',
+    		    files : [ 'resources/js/controllers/' + 'stamp.js' ]
+    		} ] );
+    	    } ]
+    	}
     } );
 
-    $routeProvider.when( '/stamp/:id/add', {
+$routeProvider.when( '/stamp/:id/add', {
+  requireAuth: true,
 	controller : 'StampAddController',
 	templateUrl : 'templates/stampAdd.html',
 	resolve : {
 	    lazy : [ '$ocLazyLoad', function( $ocLazyLoad ) {
-		return $ocLazyLoad.load( [ {
+		      return $ocLazyLoad.load( [ {
 		    name : 'COREAPI',
 		    files : [ 'resources/js/controllers/' + 'stampAdd.js' ]
 		} ] );
 	    } ]
 	}
-    } );
+} );
 
     $routeProvider.when( '/stamp/:id/del', {
-	controller : 'StampDelController',
-	templateUrl : 'templates/stampDel.html',
-	resolve : {
+      requireAuth: true,
+    	controller : 'StampDelController',
+    	templateUrl : 'templates/stampDel.html',
+    	resolve : {
 	    lazy : [ '$ocLazyLoad', function( $ocLazyLoad ) {
-		return $ocLazyLoad.load( [ {
+		    return $ocLazyLoad.load( [ {
 		    name : 'COREAPI',
 		    files : [ 'resources/js/controllers/' + 'stampDel.js' ]
-		} ] );
+		    } ] );
 	    } ]
-	}
+	    }
     } );
 
     $routeProvider.when( '/about', {
-	controller : 'AboutController',
-	templateUrl : 'templates/about.html',
-	resolve : {
-	    lazy : [ '$ocLazyLoad', function( $ocLazyLoad ) {
-		return $ocLazyLoad.load( [ {
-		    name : 'COREAPI',
-		    files : [ 'resources/js/controllers/' + 'about.js' ]
-		} ] );
-	    } ]
-	}
+      requireAuth: true,
+    	controller : 'AboutController',
+    	templateUrl : 'templates/about.html',
+    	resolve : {
+      	lazy : [ '$ocLazyLoad', function( $ocLazyLoad ) {
+      		      return $ocLazyLoad.load( [ {
+          		  name : 'COREAPI',
+          		  files : [ 'resources/js/controllers/' + 'about.js' ]
+          		} ] );
+      	} ]
+    	}
     } );
 
     $routeProvider.when( '/', {
@@ -139,38 +148,36 @@ app.config( function( $routeProvider, $httpProvider, $locationProvider ) {
     $httpProvider.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
 
     angular.isEmpty = function( value ) {
-	return angular.isUndefined( value ) || value === null || value === '';
+	     return angular.isUndefined( value ) || value === null || value === '';
     };
     angular.isTooShort = function( value, len ) {
-	return value.length <= len;
+	     return value.length <= len;
     };
 
 } );
 
 function guid() {
     function s4() {
-	return Math.floor( (1 + Math.random()) * 0x10000 ).toString( 16 ).substring( 1 );
+	     return Math.floor( (1 + Math.random()) * 0x10000 ).toString( 16 ).substring( 1 );
     }
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 };
 
-app.run( function( $rootScope, $route, $window, $location, $cookieStore, $cookies, Const, AuthService ) {
+app.run( function( $rootScope, $route, $window, $location, $cookieStore, $cookies, Const, AuthService, Redirect) {
 
-    $rootScope.$on( '$locationChangeStart', function( evt, next, current ) {
+  $rootScope.$on( '$locationChangeStart', function( evt, next, current ) {
 
 	var sessionKey = $cookieStore.get( 'token' );
 	$rootScope.isLoggedIn = $cookieStore.get( 'isLoggedIn' );
 
-	console.log('asdf');
+  var expireDate = new Date();
+
+  expireDate.setDate( expireDate.getDate() + 1 );
 
 	if ( angular.isUndefined( sessionKey ) || sessionKey === null ) {
 
 	    sessionKey = guid();
 	    $rootScope.isLoggedIn = false;
-
-	    var expireDate = new Date();
-
-	    expireDate.setDate( expireDate.getDate() + 1 );
 
 	    $cookieStore.put( 'token', sessionKey, {
 		'expires' : expireDate
@@ -180,13 +187,25 @@ app.run( function( $rootScope, $route, $window, $location, $cookieStore, $cookie
 
 	var authRes = AuthService.getSession( sessionKey );
 
-	authRes.success( function( data, status, headers, config ) {
+	authRes.success( function( data, status, headers, config, $location ) {
 
 	    // $cookieStore.put( 'isLoggedIn', data );
 
 	    $rootScope.auth = data;
 	    console.log('Response ' + data.success + ' ' + data.message);
-	    console.log('Response message' + data.message);
+
+      $rootScope.isLoggedIn = data.success;
+      $cookieStore.put( 'isLoggedIn', data.success, {
+		'expires' : expireDate
+	    } );
+
+      if(data.success == true){
+
+        $rootScope.userData = data.userData;
+
+      }
+
+      AuthService.checkAccess();
 
 	} );
 	authRes.error( function( data, status, headers, config ) {
@@ -203,32 +222,60 @@ app.run( function( $rootScope, $route, $window, $location, $cookieStore, $cookie
  *
  */
 
-app.factory( 'AuthService', function( $http, Const ) {
+app.factory( 'AuthService', function( $http, Const, $location, $rootScope, $route, Redirect ) {
 
     var getSession = function( value ) {
-	return $http.post( Const.authApiPath + '/getSession', {
-	    token : value
-	} );
+    	return $http.post( Const.authApiPath + '/getSession', {
+    	    token : value
+    	} );
     };
     var logout = function() {
-	return $http.post( Const.authApiPath + '/doLogout' );
+	     return $http.post( Const.authApiPath + '/logout' );
     };
     var getToken = function() {
       return $cookieStore.get( 'token' );
 
-    }
-    return {
-	getSession : getSession,
-	logout : logout,
-  getToken: getToken
     };
 
+    var checkAccess = function(){
+
+      var nextPath = $location.path(), nextRoute = $route.routes[nextPath];
+      if(nextRoute && nextRoute.requireAuth && !$rootScope.isLoggedIn){
+        Redirect.login();
+      }
+      else if(nextRoute && !nextRoute.requireAuth && $rootScope.isLoggedIn){
+
+        Redirect.home();
+      }
+    };
+    return {
+    	getSession : getSession,
+    	logout : logout,
+      getToken: getToken,
+      checkAccess : checkAccess
+    };
+
+
 } );
+app.factory( 'Redirect', function($location) {
+
+  var login = function(){
+
+    $location.path('/login');
+  }
+  var home = function(){
+
+    $location.path('/home');
+  }
+  return {
+    login: login,
+    home: home
+  };
+});
 
 
 
-
-app.controller( 'LogoutController', function( $scope, AuthService) {
+app.controller( 'LogoutController', function( $scope, AuthService, $location) {
   $scope.doLogout = function(){
 
     var authRes = AuthService.doLogout( sessionKey );
@@ -237,6 +284,10 @@ app.controller( 'LogoutController', function( $scope, AuthService) {
 
 
   	    console.log('Logout success');
+        //
+        Session.clear();
+        $location.path('/login');
+        //
 
   	} );
   	authRes.error( function( data, status, headers, config ) {
